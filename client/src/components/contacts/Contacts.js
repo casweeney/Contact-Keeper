93e9588a8ce2475d0bcb66f4ba8b61f688f51379
Casweeney Ojukwu <casweeno2000@gmail.com>
2021-09-28 13:59:55 +0100

    Project completed

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,36 +1,45 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
+import Spinner from '../layout/Spinner';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
 
-    const { contacts, filtered } = contactContext;
+    const { contacts, filtered, getContacts, loading } = contactContext;
 
-    if(contacts.length === 0) {
-        return <h4>Please add a contact</h4>
+    useEffect(() => {
+        getContacts();
+        // eslint-disable-next-line
+    }, []);
+
+    if(contacts !== null && contacts.length === 0 && !loading) {
+        return <h4>Please add a contact</h4>;
     }
 
     return (
         <Fragment>
-            <TransitionGroup>
-                {filtered === null ?
-                    contacts.map((contact) => {
-                        return <CSSTransition key={contact.id} timeout={500} classNames="item">
-                                    <ContactItem contact={contact} />
-                                </CSSTransition>
-                    })
-                : 
-                    filtered.map((contact) => {
-                        return <CSSTransition key={contact.id} timeout={500} classNames="item">
-                                    <ContactItem contact={contact} />
-                                </CSSTransition>
-                    })
-                }
-            </TransitionGroup>
+            { contacts !== null && !loading ? (
+                <TransitionGroup>
+                    {filtered === null ?
+                        contacts.map((contact) => {
+                            return <CSSTransition key={contact._id} timeout={500} classNames="item">
+                                        <ContactItem contact={contact} />
+                                    </CSSTransition>
+                        })
+                    : 
+                        filtered.map((contact) => {
+                            return <CSSTransition key={contact._id} timeout={500} classNames="item">
+                                        <ContactItem contact={contact} />
+                                    </CSSTransition>
+                        })
+                    }
+                </TransitionGroup>
+            ) : <Spinner /> }
+            
         </Fragment>
-    )
+    );
 }
 
 export default Contacts;
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,26 +1,51 @@
 import {
+    GET_CONTACTS,
     ADD_CONTACT,
     DELETE_CONTACT,
     SET_CURRENT,
     CLEAR_CURRENT,
     UPDATE_CONTACT,
     FILTER_CONTACTS,
-    CLEAR_FILTER
+    CLEAR_FILTER,
+    CLEAR_CONTACTS,
+    CONTACT_ERROR
 } from '../types';
 
 // eslint-disable-next-line
 export default (state, action) => {
     switch(action.type) {
+        case GET_CONTACTS:
+            return {
+                ...state,
+                contacts: action.payload,
+                loading: false
+            }
         case ADD_CONTACT:
             return {
                 ...state,
-                contacts: [...state.contacts, action.payload]
+                contacts: [action.payload, ...state.contacts],
+                loading: false
+            };
+        case UPDATE_CONTACT:
+            return {
+                ...state,
+                contacts: state.contacts.map((contact) => { return contact._id === action.payload._id ? action.payload : contact}),
+                loading: false
             };
         case DELETE_CONTACT:
             return {
                 ...state,
-                contacts: state.contacts.filter(contact => contact.id !== action.payload)
+                contacts: state.contacts.filter(contact => contact._id !== action.payload),
+                loading: false
             };
+        case CLEAR_CONTACTS:
+            return {
+                ...state,
+                contacts: null,
+                filtered: null,
+                error: null,
+                current: null
+            }
         case SET_CURRENT:
             return {
                 ...state,
@@ -30,11 +55,6 @@ export default (state, action) => {
             return {
                 ...state,
                 current: null
-            };
-        case UPDATE_CONTACT:
-            return {
-                ...state,
-                contacts: state.contacts.map((contact) => { return contact.id === action.payload.id ? action.payload : contact})
             };
         case FILTER_CONTACTS:
             return {
@@ -48,6 +68,11 @@ export default (state, action) => {
             return {
                 ...state,
                 filtered: null
+            };
+        case CONTACT_ERROR:
+            return {
+                ...state,
+                error: action.payload
             };
         default:
             return state
